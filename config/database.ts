6de8d95cca2829002d18354fb4c8cb436c56fd6f
Pritee <pritee@server.com>
2022-11-29 12:12:37 +0530

    services API

--- config/database.ts
+++ config/database.ts
@@ -2,11 +2,11 @@ export default ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
+      host: env('DATABASE_HOST', '192.168.0.204'),
       port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'nextstrapi'),
-      user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', 'Root@123'),
+      database: env('DATABASE_NAME', 'Next_Strapi'),
+      user: env('DATABASE_USERNAME', 'cccsql'),
+      password: env('DATABASE_PASSWORD', 'ccc123'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
